9a2ad83a1ab5e611af3190422c4c551c95c5aad2
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-04-15 10:13:47 +0530

    Add files via upload

---
+++ Implementation/inc/header.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct voter
+{
+long int adhar, vid;
+char name[20], father[20], spouse[20], place[20];
+int age;
+}voter;
+
+typedef struct cand
+{
+int num;
+char name[20];
+}cand;
+
+void Exit();
+void Admin();
+voter read();
+
